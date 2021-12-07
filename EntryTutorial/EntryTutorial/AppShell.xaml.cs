52617fb4de27bec58c949d2d207f78fa8f5dce7f
User <User@WIN-C9HQIV4VA1U>
2021-12-07 22:11:24 +0300

    Add project files.

---
+++ EntryTutorial/EntryTutorial/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using EntryTutorial.ViewModels;
+using EntryTutorial.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace EntryTutorial
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ EntryTutorial/EntryTutorial/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EntryTutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
